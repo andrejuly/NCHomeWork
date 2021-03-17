1dd4c73b60967b93a5f28ff7f41a923d43a52cd6
Andrey Ulyanov <andrejuly@yandex.ru>
2021-03-17 21:14:57 +0300

    Create Project

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import com.ulyanovcompany.task1.Circle;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println(new Circle());
+    }
+}
