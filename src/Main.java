b7846583ba3e5c59d6dc34210e7ec54a3466e36d
Andrey Ulyanov <andrejuly@yandex.ru>
2021-03-17 21:52:45 +0300

    Add Employee.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import com.ulyanovcompany.task1.Circle;
+import com.ulyanovcompany.task1.Employee;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println(new Circle());
+        System.out.println(new Employee(1,"andrey", "anov", 12222));
     }
 }
